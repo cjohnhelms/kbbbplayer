368e3f2f5d2c3b4f504bc14ad8d28a6ada83d612
Christopher Helms <cjohnhelms@gmail.com>
2025-04-02 17:58:54 -0500

    one file

--- app/app.py
+++ app/app.py
@@ -2,11 +2,24 @@
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from time import sleep
 
 reader = SimpleMFRC522()
+GPIO.setwarnings(False)
 
-try:
-        id, _ = reader.read()
-        print(id)
-finally:
-        GPIO.cleanup()
+spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-modify-playback-state"))
+
+ALBUMS = {
+    "584192607192": "spotify:album:7FWCgfnTgupXdyBy51ME9m"
+}
+
+while True:
+    try:
+            id, _ = reader.read()
+            if id in ALBUMS.keys():
+                spotify.start_playback(context_uri=hm_uri)
+    finally:
+            GPIO.cleanup()
+            sleep(10)
