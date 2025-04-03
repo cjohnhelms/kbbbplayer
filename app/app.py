99d4c7f4c9cb8abc42746c5f9fb3ae8d09e9337f
Christopher Helms <cjohnhelms@gmail.com>
2025-04-02 20:58:20 -0500

    try again

--- app/app.py
+++ app/app.py
@@ -11,18 +11,17 @@ reader = SimpleMFRC522()
 GPIO.setwarnings(False)
 
 spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-modify-playback-state"))
-spotify.start_playback(context_uri="spotify:album:7FWCgfnTgupXdyBy51ME9m")
 
 ALBUMS = {
     584192607192: "spotify:album:7FWCgfnTgupXdyBy51ME9m"
 }
 
-#while True:
-#    try:
-#            id, _ = reader.read()
-#            if id in ALBUMS.keys():
-#                logging.info("Found album")
-#                spotify.start_playback(context_uri=ALBUMS[id])
-#    finally:
-#            GPIO.cleanup()
-#            sleep(10)
+while True:
+    try:
+            id, _ = reader.read()
+            if id in ALBUMS.keys():
+                logging.info("Found album")
+                spotify.start_playback(context_uri=ALBUMS[id])
+    finally:
+            GPIO.cleanup()
+            sleep(10)
