c8c225eecc21acf84a81ab45d6299566ffa6fb2d
Christopher Helms <cjohnhelms@gmail.com>
2025-04-05 14:45:38 -0500

    updates

--- app/app.py
+++ app/app.py
@@ -6,7 +6,6 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from time import sleep
 import logging
-from datetime import datetime
 
 reader = SimpleMFRC522()
 GPIO.setwarnings(False)
@@ -20,17 +19,16 @@ APPLE_TV_ID = "6F2A5C54-CEB8-430F-A6B3-7E31DCE85846"
 
 previous = 0
 
-while True:
-    try:
-            id, _ = reader.read()
-            logging.info(f"ID: {id}")
-            if id in ALBUMS.keys() and id != previous:
-                logging.info("Found album")
-                spotify.transfer_playback(APPLE_TV_ID, False)
-                spotify.start_playback(context_uri=ALBUMS[id])
-                previous = id
-    except Exception as e:
-          logging.error(e)
-    finally:
-            GPIO.cleanup()
-            sleep(2)
+try:
+    while True:
+        id, _ = reader.read()
+        logging.info(f"ID: {id}")
+        if id in ALBUMS.keys() and id != previous:
+            logging.info("Found album")
+            spotify.transfer_playback(APPLE_TV_ID, False)
+            spotify.start_playback(context_uri=ALBUMS[id])
+            previous = id
+        sleep(2)
+except KeyboardInterrupt:
+        GPIO.cleanup()
+        raise
