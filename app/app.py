449d3ea2b1b2a5c55da26b49c0434f4bf701c3bb
Christopher Helms <cjohnhelms@gmail.com>
2025-04-05 15:11:18 -0500

    testing

--- app/app.py
+++ app/app.py
@@ -6,9 +6,6 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from time import sleep
 import logging
-from datetime import datetime
-
-spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-modify-playback-state"))
 
 ALBUMS = {
     584192607192: "spotify:album:7FWCgfnTgupXdyBy51ME9m"
@@ -17,21 +14,9 @@ APPLE_TV_ID = "6F2A5C54-CEB8-430F-A6B3-7E31DCE85846"
 
 previous = 0
 
-#try:
-    #while True:
-        #id, _ = reader.read()
-        #logging.info(f"ID: {id}")
-        #if id in ALBUMS.keys() and id != previous:
-            #logging.info("Found album")
-            #spotify.transfer_playback(APPLE_TV_ID, False)
-            #spotify.start_playback(context_uri=ALBUMS[id])
-            #previous = id
-        #sleep(2)
-#except KeyboardInterrupt:
-        #GPIO.cleanup()
-        #raise
-
 reader = MFRC522()
+GPIO.setwarnings(False)
+spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-modify-playback-state"))
 
 try:
     while True:
